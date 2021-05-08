351f36289c2dc3ef328cd22edc1059e4c32ff212
Alisa Morenko <Alisa.Morenko@gmail.com>
2021-05-08 19:35:06 +0300

    1.0

--- timer.js
+++ timer.js
@@ -1,29 +1,52 @@
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+const refs = {
+  clockface: document.querySelector('#timer-1'),
+  dd: document.querySelector('[data-value="days"]'),
+  hh: document.querySelector('[data-value="hours"]'),
+  mm: document.querySelector('[data-value="mins"]'),
+  ss: document.querySelector('[data-value="secs"]'),
+};
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  start() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      const time = this.getTimeComponents(deltaTime);
+      updateClockface(time);
+    }, 1000);
+  }
 
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
 
-new CountdownTimer({
-  selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2019"),
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+}
+
+function updateClockface({ days, hours, mins, secs }) {
+  refs.dd.textContent = `${days}`;
+  refs.hh.textContent = `${hours}`;
+  refs.mm.textContent = `${mins}`;
+  refs.ss.textContent = `${secs}`;
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 17, 2022'),
 });
+
+timer.start();
