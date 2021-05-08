3b89ea46945c5b92277b0dd0a3b105184259d3d2
Alisa Morenko <Alisa.Morenko@gmail.com>
2021-05-08 19:53:18 +0300

    1.1

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 const refs = {
-  clockface: document.querySelector('#timer-1'),
+  clockface: document.querySelector("#timer-1"),
   dd: document.querySelector('[data-value="days"]'),
   hh: document.querySelector('[data-value="hours"]'),
   mm: document.querySelector('[data-value="mins"]'),
@@ -22,13 +22,13 @@ class CountdownTimer {
   }
 
   pad(value) {
-    return String(value).padStart(2, '0');
+    return String(value).padStart(2, "0");
   }
 
   getTimeComponents(time) {
     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
     const hours = this.pad(
-      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     );
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
@@ -45,8 +45,8 @@ function updateClockface({ days, hours, mins, secs }) {
 }
 
 const timer = new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2022'),
+  selector: "#timer-1",
+  targetDate: new Date("Jul 17, 2022"),
 });
 
 timer.start();
